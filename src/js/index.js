920dda6d7d3dc98fa4ac24d6def01b8fd8619905
bas <bas@ya.ru>
2020-11-18 17:01:09 +0300

    First

--- src/js/index.js
+++ src/js/index.js
@@ -5,4 +5,58 @@ window.$ = $;
 
 $(function() {
 
+    // Прикрепляем шапку
+    var header = $('.main-header');
+    var offset = header.offset().top;
+
+    $(window).scroll(function() {
+
+        if ($(this).scrollTop() > offset)
+        {
+            header.addClass('main-header_fixed');
+        }
+        else
+        {
+            header.removeClass('main-header_fixed');
+        }
+    });
+    
+    // Отслеживание прокрутки страницы
+    $(window).scroll(function(){
+
+        var scroll = $(window).scrollTop();
+
+        var sections = $('.block-point');
+
+        sections.each(function(i, el) {
+
+            var top  = $(el).offset().top - 100;
+            var bottom = top + $(el).height();
+            var id = $(el).attr('id');
+
+            if (scroll > top && scroll < bottom)
+            {
+                $('.menu-top__link_active').removeClass('menu-top__link_active');
+                $('a[href="#'+id+'"]').addClass('menu-top__link_active');
+            }
+        });
+
+        if ($('body').height() - 10 < ($(window).height() + scroll))
+        {
+            $('.menu-top__link_active').removeClass('menu-top__link_active');
+            $('.menu-top:last-child').addClass('menu-top__link_active');
+        }
+    });
+
+    // Переход по ссылкам меню
+    $("nav").on("click", "a", function (event) {
+
+        event.preventDefault();
+
+        var id = $(this).attr('href'),
+
+        top = $(id).offset().top - 60;
+
+        $('body,html').animate({scrollTop: top}, 800);
+    });
 });
